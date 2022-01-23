b4a758a37fb97eb4e4626ce83f0ed208f651f8f2
Ib227 <98104063+Ib227@users.noreply.github.com>
2022-01-23 18:54:03 +0200

    Add files via upload

---
+++ install.py
@@ -0,0 +1,11 @@
+import os
+import sys
+os.system('clear')
+os.system('tremux-setup-storage')
+os.system('cd storage')
+os.system('cd Download')
+os.system('cp insta-hack.py $HOME')
+os.system('cd')
+os.system('apt install python') 
+os.system('pip3 \ pip \ pip2  install  requests secrets manpyfile fakepyfile uuid4 -y ')
+os.system('python3 insta-hack.py')
